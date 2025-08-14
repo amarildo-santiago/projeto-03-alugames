b4b4c84803d0a34b4a668bc4baf64b27088a27bc
Amarildo Santhiago <a.santhiagosoares@gmail.com>
2025-08-14 12:07:49 -0300

    Versão 02 Jogos Alugados

--- js/app.js
+++ js/app.js
@@ -1,16 +1,29 @@
+let jogosAlugados = 1;
+
+function exibirAlugueisConsole(){
+    console.log(`A quantidade de jogos alugados é ${jogosAlugados}!`)
+}
+
+
 function alterarStatus(id){
     let banner = document.getElementById(`game-${id}`);
     let imagem = banner.querySelector('.dashboard__item__img');
     let botao = banner.querySelector('.dashboard__item__button');
 
     if (imagem.classList.contains('dashboard__item__img--rented')){
+        alert('Você tem certeza que deseja devolver o jogo?')
         imagem.classList.remove('dashboard__item__img--rented');
         botao.classList.remove('dashboard__item__button--return');
         botao.textContent = 'Alugar';
+        jogosAlugados--
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
         botao.textContent = 'Devolver';
+        jogosAlugados++
     }
+
+    exibirAlugueisConsole();
     
 }
+
